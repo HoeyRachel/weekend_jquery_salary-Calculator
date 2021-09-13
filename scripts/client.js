f156648598d7f2df1390e4d368541dd87cc641d2
Rachel Hoey <rhoey@hotmail.com>
2021-09-12 20:20:35 -0500

    input working

--- scripts/client.js
+++ scripts/client.js
@@ -2,9 +2,22 @@ $(document).ready (onReady);
 
 function onReady() {
     console.log ('in onReady');
-    $(`submitbutton`).on (`click`, getData);
+    $(`#submitButton`).on (`click`, getData);
 } //end onReady
 
+let employees = [];
+
 function getData (){
+    console.log('in getData');
+    //get user input & save in object
+    let newEntry ={
+        firstName : $(`#FirstName`).val(),
+        lastName : $(`#LastName`).val(),
+        IDNumber : $(`#IDNumber`).val(),
+        JobTitle : $(`#JobTitle`).val(),
+        AnnualSalary : $(`#AnnualSalary`).val(),
+    }//end newEntry
+    //push item into an array
+    employees.push(newEntry);
 
 }
