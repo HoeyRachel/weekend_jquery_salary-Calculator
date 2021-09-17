58d5eb12fcd11f9caf2f855bedf6798e1c9b3ee0
Rachel Hoey <rhoey@hotmail.com>
2021-09-16 19:51:39 -0500

    fixed code

--- scripts/client.js
+++ scripts/client.js
@@ -9,6 +9,12 @@ function onReady() {
     $( '#employeeTable' ).on( 'click', '.deleteEmployee', deleteEmployee );
 } //end onReady
 
+let formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    maximumFractionDigits: 0,
+});
+
 
 function getData (){
     console.log('in getData');
@@ -35,29 +41,30 @@ function getData (){
         </tr>`)
     for (i=0; i<employees.length; i++){
         el.append( `<tr><td>${ employees[i].firstName }</td>, <td>${ employees[i].lastName}</td>,  <td>${ employees[i].IDNumber}</td>,
-         <td>${ employees[i].JobTitle }</td>, <td>${ employees[i].AnnualSalary}</td>
+        <td>${ employees[i].JobTitle }</td>, <td>${formatter.format(employees[i].AnnualSalary)}</td>
          <td><button class="deleteEmployee">Delete</button>
          </td></tr>` );
-
     } 
-    let monthlyWages=0;
-    let sum = 0;
-    for (i=0; i<employees.length; i++){
-        monthlyWages = (sum +=Number(employees[i].AnnualSalary))/12;
-        if (monthlyWages>20000){
-            $(`#salaryCalculator`).toggleClass ( 'red');
-        }
-    } 
-    
-    el.append (monthlyWages);
+    el.append ();
     $(`#FirstName`).val('');
     $(`#LastName`).val('');
     $(`#IDNumber`).val('');
     $(`#JobTitle`).val('');
     $(`#AnnualSalary`).val('');
-    $(`#salaryCalculator`).val('');
-    
-    
+
+    let monthlyWages=0;
+    let sum = 0;
+    for (i=0; i<employees.length; i++){
+        monthlyWages = (sum +=Number(employees[i].AnnualSalary))/12;
+        if (monthlyWages>20000){
+            // $(`#salaryCalculator`).addClass ( 'red');
+            $(`#salaryCalculator`).css( "background-color", "red");
+        }
+    } 
+    let el2 = $(`#salaryCalculator`);
+    el2.empty();
+    el2.append (`Total Monthly Salary  ${formatter.format(monthlyWages)}`);
+
 }
 
 function deleteEmployee(){
